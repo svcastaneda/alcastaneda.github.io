9d3a2f338ef90e21c13c256bb73d3b3db927899f
Adriana Castaneda <adriana.lcs316@gmail.com>
2016-03-16 22:33:12 -0500

    add ajax to main page to render hotel page

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#hotel").on('click', function(event) {
+
+      event.preventDefault();
+
+        $.ajax({
+            url: "http://alcastaneda.github.io/hotel.html",
+            dataType: "html"
+         }).done(function(data) {
+            var bod = $(data).children();
+            console.log(bod);
+            $(".content").html(bod);
+         });
+  });
+
+
+});
