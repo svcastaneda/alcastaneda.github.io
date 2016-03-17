cc92e7bcea3cf8f88ef0d0694ae3e071c9796101
Adriana Castaneda <adriana.lcs316@gmail.com>
2016-03-17 10:20:32 -0500

    add get prices ajax

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 
-  $("#hotel").on('click', function(event) {
-      $(".index").hide();
-      event.preventDefault();
-      getHotel();
-  });
-
+    $("#hotel").on('click', function(event) {
+          $(".index").hide();
+          event.preventDefault();
+          getHotel();
+    });
 });
 
 
@@ -18,5 +17,23 @@ function getHotel(){
             var bod = $(data).children();
             console.log(bod);
             $(".content").html(bod);
+            getPrices();
          });
+};
+
+
+function getPrices(){
+      $("#prices").on('click', function(event) {
+          console.log("clicked");
+              $(".index").hide();
+              event.preventDefault();
+              $.ajax({
+                url: "http://alcastaneda.github.io/prices.html",
+                dataType: "html"
+             }).done(function(data) {
+                var bod = $(data).children();
+                console.log(bod);
+                $(".content").html(bod);
+             });
+        });
 };
